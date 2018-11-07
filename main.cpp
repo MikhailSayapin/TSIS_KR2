3d9374736053f44d32e95310745aa5fe55fb40e4
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2018-11-07 12:05:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,8 +26,8 @@ int main()
 	double x[4], y[4];
 	for (int i = 0; i < 4; i++) //Заполняем массив аргументов случайными значениями
 	{
-		x[i] = rand();
-		y[i] = rand();
+		x[i] = rand()%100;
+		y[i] = rand()%100;
 	}
 
 	Chromosom c1(x[0], y[0]);
@@ -41,4 +41,46 @@ int main()
 	v.push_back(c4);
 
 	int N = 1;
-	while (N<100)
+	while (N <= 100)
+	{
+		sort(v.begin(), v.end(), [](Chromosom &c1, Chromosom &c2)
+		{
+			return c1.value > c2.value;
+		});
+		x[3] = x[0];
+		swap(y[0], y[1]);
+		y[3] = y[2];
+		y[2] = y[1];
+
+		y[rand() % 4] += rand()%2;
+		x[rand() % 4] += rand()%2; // Мутация случайной хромосомы
+
+		for (int i = 0; i < 4; i++)
+		{
+			cout << x[i] << "\t";
+		}
+		cout << endl;
+		for (int i = 0; i < 4; i++)
+		{
+			cout << y[i] << "\t";
+		}
+		cout << endl;
+		for (int i = 0; i < 4; i++)
+		{
+			cout << f(x[i], y[i]) << "\t";
+		}
+		cout << endl;
+		cout << "=================================" << endl;
+		Chromosom c1(x[0], y[0]);
+		Chromosom c2(x[1], y[1]);
+		Chromosom c3(x[2], y[2]);
+		Chromosom c4(x[3], y[3]);
+		vector<Chromosom> v;
+		v.push_back(c1);
+		v.push_back(c2);
+		v.push_back(c3);
+		v.push_back(c4);
+
+		N++;
+	}
+}
